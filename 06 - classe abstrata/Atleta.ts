93f9df4a045f230322ab9797ed3c53857c7527ed
Beatriz <alvesbeatriz148@gmail.com>
2024-03-28 14:08:16 -0300

    feat: prática sobre classe abstrata (POO)

---
+++ 06 - classe abstrata/Atleta.ts
@@ -0,0 +1,11 @@
+import { Pessoa } from "./Pessoa"
+
+export abstract class Atleta extends Pessoa {
+    constructor(nome: string) {
+        super(nome)
+    }
+
+    public aquecer(): void{
+    console.log(`Aquecendo... `);
+    }
+}
---
+++ 06 - classe abstrata/Nadador.ts
@@ -0,0 +1,15 @@
+import { Atleta } from "./Atleta"
+
+export class Nadador extends Atleta {
+    constructor (nome: string){
+        super(nome)
+    }
+
+    public cansou(): void{
+        console.log(`Cansei de nadar!`)
+    }
+
+    public nadar(): void{
+        console.log(`Nadando ...`)
+    }
+}
