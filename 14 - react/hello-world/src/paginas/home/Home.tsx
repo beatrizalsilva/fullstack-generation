ab8cb833c3f22e615ef9bfc0b6c1df18ad08b29e
Beatriz <alvesbeatriz148@gmail.com>
2024-05-15 12:42:02 -0300

    feat: inciando conteúdo de react

---
+++ 14 - react/hello-world/src/paginas/home/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./Home.css"
+
+function Home() {
+    return (
+          <>
+            <h1 className='titulo'>Hello World!</h1>
+            <img src="https://cdn.freebiesupply.com/logos/large/2x/react-1-logo-png-transparent.png" alt="Logo do React" className='img'/>
+          </>
+    );
+}
+
+export default Home;
