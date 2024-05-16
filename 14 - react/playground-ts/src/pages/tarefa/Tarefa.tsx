d26528107d4ac6ee14100e30646d83c1f65b0cb2
Beatriz <alvesbeatriz148@gmail.com>
2024-05-16 12:21:33 -0300

    feat: props e hooks no React

---
+++ 14 - react/playground-ts/src/pages/tarefa/Tarefa.tsx
@@ -0,0 +1,26 @@
+﻿import { useEffect, useState } from "react";
+
+function Tarefa() {
+  const [completed, setCompleted] = useState(false);
+  const [tarefa, setTarefa] = useState("");
+
+  useEffect(() => {
+    if (completed) {
+      setTarefa("Parabéns! Tarefa concluída")
+    } [completed]
+  })
+
+  return (
+    <>
+      <div>
+        <h2>Componente Tarefa</h2>
+        <h3>{tarefa}</h3>
+        <p>Conclua a Tarefa</p>
+
+        <button onClick={() => setCompleted(true)}> Concluir tarefa </button>
+      </div>
+    </>
+  );
+}
+
+export default Tarefa
